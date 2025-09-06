1aaa77104e3cc3e6c8cd14c2ff802e35909e0109
Ahkar Kyaw <ahkar12694@gmail.com>
2025-09-06 16:13:26 +0630

    Covid Portfolio Project actual scripts in video.sql

--- Covid Portfolio Project actual scripts in video.sql
+++ Covid Portfolio Project actual scripts in video.sql
@@ -1,46 +1,46 @@
 Select *
-From PortfolioProject..CovidDeaths
+From PortfoiloProject..CovidDeaths
 Where continent is not null 
 order by 3,4
 
 Select Location, date, total_cases, new_cases, total_deaths, population
-From PortfolioProject..CovidDeaths
+From PortfoiloProject..CovidDeaths
 Where continent is not null 
 order by 1,2
 
 Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
-From PortfolioProject..CovidDeaths
+From PortfoiloProject..CovidDeaths
 Where location like '%states%'
 and continent is not null 
 order by 1,2
 
 Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
+From PortfoiloProject..CovidDeaths
 --Where location like '%states%'
 order by 1,2
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
+From PortfoiloProject..CovidDeaths
 --Where location like '%states%'
 Group by Location, Population
 order by PercentPopulationInfected desc
 
 Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
-From PortfolioProject..CovidDeaths
+From PortfoiloProject..CovidDeaths
 --Where location like '%states%'
 Where continent is not null 
 Group by Location
 order by TotalDeathCount desc
 
 Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
-From PortfolioProject..CovidDeaths
+From PortfoiloProject..CovidDeaths
 --Where location like '%states%'
 Where continent is not null 
 Group by continent
 order by TotalDeathCount desc
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
-From PortfolioProject..CovidDeaths
+From PortfoiloProject..CovidDeaths
 --Where location like '%states%'
 where continent is not null 
 --Group By date
@@ -49,8 +49,8 @@ order by 1,2
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
 --, (RollingPeopleVaccinated/population)*100
-From PortfolioProject..CovidDeaths dea
-Join PortfolioProject..CovidVaccinations vac
+From PortfoiloProject..CovidDeaths dea
+Join PortfoiloProject..CovidVaccinations vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null 
