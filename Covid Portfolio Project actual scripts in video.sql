7f24f5ebb1644cac811648349da72613dcf2b2cd
Ahkar Kyaw <ahkar12694@gmail.com>
2025-09-06 16:20:22 +0630

    Covid Portfolio Project actual scripts in video.sql

--- Covid Portfolio Project actual scripts in video.sql
+++ Covid Portfolio Project actual scripts in video.sql
@@ -104,8 +104,8 @@ Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinatio
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, 
   dea.Date) as RollingPeopleVaccinated
 --, (RollingPeopleVaccinated/population)*100
-From PortfolioProject..CovidDeaths dea
-Join PortfolioProject..CovidVaccinations vac
+From PortfoiloProject..CovidDeaths dea
+Join PortfoiloProject..CovidVaccinations vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null 
